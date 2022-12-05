951bb10d2cf6fa2cfbd5c3040f277024dee06f4a
namlvph18979 <109219569+namlvph18979@users.noreply.github.com>
2022-12-05 16:42:40 +0700

    a

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ app.use('/public',express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 let cors = require("cors");
 app.use(cors());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var url = require('url');
 var sharp = require('sharp');
 
 const wallpaperSchema = new Schema({
+
   id: String,
   title: String,
   desc: String,
@@ -77,70 +78,37 @@ router.get('/lists/:page', function(req, res, next) {
 router.get('/upload', function(req, res, next) {
   res.render('upload', { title: 'Upload' });
 });
-router.post('/upload', imageUpload.single('image'),function(req, res, next) {
-  sharp(req.file.path).resize(150, 150).toFile('./public/upload/'+ '150x150-'+req.file.filename, function(err) {
-    if (err) {
-      console.error('sharp>>>', err);
-    }
-    console.log('ok okoko')
-  })
-  var obj = {
-    id: req.body.id,
-    title: req.body.name,
-    desc: req.body.desc,
-    img: {
-      url: 'public/upload/' + req.file.filename,
-      contentType: req.file.mimetype,
-      thumbnail: 'public/upload/150x150-' + req.file.filename
-    },
-    createDate: Date.now()
-  }
-  imgModel.create(obj, (err, item) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      // item.save();
-      res.redirect('/');
-    }
-  });
-  // imgModel.findOne({ name: req.body.name }, function (err, img) {
-  //   if (img._id == null){
-  //     var obj = {
-  //       id: req.body.id,
-  //       title: req.body.name,
-  //       desc: req.body.desc,
-  //       img: {
-  //         url: path.join(__dirname, '../' + '/upload/' + req.file.filename),
-  //         contentType: req.file.mimetype
-  //       },
-  //       createDate: Date.now()
-  //     }
-  //     imgModel.create(obj, (err, item) => {
-  //       if (err) {
-  //         console.log(err);
-  //       }
-  //       else {
-  //         // item.save();
-  //         res.redirect('/');
-  //       }
-  //     });
-  //   } else {
-  //     imgModel.updateOne({_id: img._id}, {
-  //       id: req.body.id,
-  //       title: req.body.name,
-  //       desc: req.body.desc,
-  //       img: {
-  //         url: path.join(__dirname, '../' + '/upload/' + req.file.filename),
-  //         contentType: req.file.mimetype
-  //       },
-  //       createDate: Date.now()
-  //     }).then(data => {
-  //       if (data != null) res.send("Update thanh cong~!!!!")
-  //     })
-  //   }
-  // });
-});
+// router.post('/upload', imageUpload.single('image'),function(req, res, next) {
+//   sharp(req.file.path).resize(150, 150).toFile('./public/upload/'+ '150x150-'+req.file.filename, function(err) {
+//     if (err) {
+//       console.error('sharp>>>', err);
+//     }
+//     console.log('ok okoko')
+//   })
+//   var obj = {
+//     id: req.body.id,
+//     title: req.body.name,
+//     desc: req.body.desc,
+//     img: {
+//       url: 'public/upload/' + req.file.filename,
+//       contentType: req.file.mimetype,
+//       thumbnail: 'public/upload/150x150-' + req.file.filename
+//     },
+//     createDate: Date.now()
+//   }
+//   imgModel.create(obj, (err, item) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     else {
+//       // item.save();
+//       res.redirect('/');
+//     }
+//   });
+
+
+
+// });
 router.get('/lists_json/:page', function(req, res, next) {
   let perPage = 10;
   let page = req.params.page || 1;
@@ -154,5 +122,19 @@ router.get('/lists_json/:page', function(req, res, next) {
           res.send({items: items,currentPage: page});
         });
       });
+});
+
+router.get('/api', async function(req, res, next) {
+  const Img = mongoose.model('wallpaper', wallpaperSchema);
+
+  const PAGE_SIZE = 5;
+  var page = req.query.page;
+  page = parseInt(page);
+  if (page < 1) {
+    page = 1;
+  }
+  var skip = (page - 1)*PAGE_SIZE;
+
+  await Img.find({}).skip(skip).limit(5).then(data => res.status(200).send(data))
 });
 module.exports = router;
